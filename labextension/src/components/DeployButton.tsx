b01a20b58b5c6fc544c8a5b8fc0a9f266e3a7b72
zhoujinyu <2319109590@qq.com>
2026-02-21 08:53:54 +0800

    lint fix
    Signed-off-by: zhoujinyu <2319109590@qq.com>

--- labextension/src/components/DeployButton.tsx
+++ labextension/src/components/DeployButton.tsx
@@ -29,24 +29,24 @@ import { styled } from '@mui/material/styles';
 const DeployButtonContainer = styled('div')({
   '& .deploy-button': {
     // Add any specific styling for the deploy button container
-  }
+  },
 });
 
 const StyledButtonGroup = styled(ButtonGroup)({
-  width: '100%'
+  width: '100%',
 });
 
 const MainButton = styled(Button)({
   width: '100%',
-  whiteSpace: 'nowrap'
+  whiteSpace: 'nowrap',
 });
 
 const DropdownButton = styled(Button)({
-  width: 'auto'
+  width: 'auto',
 });
 
 const StyledPopper = styled(Popper)({
-  zIndex: 2
+  zIndex: 2,
 });
 
 interface ISplitDeployButton {
@@ -65,15 +65,15 @@ export const SplitDeployButton: React.FunctionComponent<
   const options = [
     {
       label: 'Compile and Run',
-      value: 'run'
+      value: 'run',
     },
     { label: 'Compile and Upload', value: 'upload' },
-    { label: 'Compile and Save', value: 'compile' }
+    { label: 'Compile and Save', value: 'compile' },
   ];
 
   const handleMenuItemClick = (
     event: React.MouseEvent<HTMLLIElement>,
-    index: number
+    index: number,
   ) => {
     setSelectedIndex(index);
     setOpen(false);
@@ -142,7 +142,7 @@ export const SplitDeployButton: React.FunctionComponent<
                   {...TransitionProps}
                   style={{
                     transformOrigin:
-                      placement === 'bottom' ? 'center top' : 'center bottom'
+                      placement === 'bottom' ? 'center top' : 'center bottom',
                   }}
                 >
                   <Paper id="menu-list-grow">
